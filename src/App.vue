6927b3454da37cc50caf7870feee65d78861200a
Peter Kazazes <peter@peterk.co>
2019-01-29 23:26:48 -0800

    no router for now

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,40 @@
         <span class="red--text darken-4">Sibyl Vision</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat>SkyScraper</v-btn>
+      <v-toolbar-items id="toolbar-menu">
+        <v-btn flat data-menuanchor="skyscraper">SkyScraper</v-btn>
+        <v-btn flat data-menuanchor="rfCapture">RF Capture</v-btn>
+        <v-btn flat data-menuanchor="industries">Applications</v-btn>
+        <v-btn flat data-menuanchor="pricing">Pricing</v-btn>
+        <v-btn flat data-menuanchor="about">About</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <div id="fullpage">
+        <div class="section">
+          <h3>Intro to SDR & SkyScraper</h3>
+        </div>
         <div class="section" id="heroWrapper">
           <Hero></Hero>
         </div>
         <div class="section">
           <v-container fill-height color="pink">
             <v-layout fill-height color="pink">
-              <h3>Peter Kazazes</h3>
+              <h3>Applications</h3>
+            </v-layout>
+          </v-container>
+        </div>
+        <div class="section">
+          <v-container fill-height color="pink">
+            <v-layout fill-height color="pink">
+              <h3>Pricing</h3>
+            </v-layout>
+          </v-container>
+        </div>
+        <div class="section">
+          <v-container fill-height color="pink">
+            <v-layout fill-height color="pink">
+              <h3>About Us</h3>
             </v-layout>
           </v-container>
         </div>
@@ -44,7 +65,8 @@ export default class App extends Vue {
   private mounted() {
     const a = new this.fullpage("#fullpage", {
       navigation: true,
-      scrollOverflow: true
+      menu: "#toolbar-menu",
+      anchors: ["skyscraper", "rfCapture", "industries", "pricing", "about"]
     });
   }
 
@@ -54,6 +76,14 @@ export default class App extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss">
+#toolbar-menu {
+  .active {
+    position: relative;
+    &::before {
+      background-color: currentColor;
+    }
+  }
+}
 </style>
 
