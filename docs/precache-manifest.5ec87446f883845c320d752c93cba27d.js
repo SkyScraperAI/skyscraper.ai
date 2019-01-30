b57888d3156a6bc17d0daa4b3bcd7be0fa5ed0e5
Peter Kazazes <peter@peterk.co>
2019-01-29 23:37:14 -0800

    +docs dir

---
+++ docs/precache-manifest.5ec87446f883845c320d752c93cba27d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d352c54ebdab36ff9b38df6bc300643",
+    "url": "/vendor/scrolloverflow.js"
+  },
+  {
+    "revision": "5cb5795cf3bf3b3657be51f8c6b7655a",
+    "url": "/vendor/easings.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "94bc7f0bbd72352db683",
+    "url": "/js/chunk-vendors.9033aedb.js"
+  },
+  {
+    "revision": "fa62846562e62e67caeb",
+    "url": "/js/app.8d187165.js"
+  },
+  {
+    "revision": "d104d22820d8039189d0b0f056ca212a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94bc7f0bbd72352db683",
+    "url": "/css/chunk-vendors.521fc418.css"
+  },
+  {
+    "revision": "fa62846562e62e67caeb",
+    "url": "/css/app.5f4cd3ed.css"
+  }
+];
