83d61f67986f3d6f4b0e827ed26f2685389f20ec
Peter Kazazes <peter@peterk.co>
2019-03-07 00:46:24 -0800

    update twitter handle

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,66 +1,52 @@
 <template>
-    <v-footer
-            height="auto"
-            color="red lighten-1"
-            class="pb-1"
-    >
-        <v-layout
-                justify-center
-                row
-                wrap
-                class="py-3"
-        >
-            <div v-for="link in links" :key="link.label">
-                <v-btn
-                        color="white"
-                        flat
-                        round
-                        :href="link.url ? link.url : '#'"
-                >
-                    <span class="white--text monospaced">{{ link.label }}</span>
-                </v-btn>
-            </div>
-        </v-layout>
-    </v-footer>
+  <v-footer height="auto" color="red lighten-1" class="pb-1">
+    <v-layout justify-center row wrap class="py-3">
+      <div v-for="link in links" :key="link.label">
+        <v-btn color="white" flat round :href="link.url ? link.url : '#'">
+          <span class="white--text monospaced">{{ link.label }}</span>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-footer>
 </template>
 
 <script>
-    import Vue from "vue";
-    import { Component } from "vue-property-decorator";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-    @Component({
-        name: "Footer",
-        data() {
-            return {
-                links: [
-                    {
-                        label: "(415) 967-5238",
-                        url: "tel:+14159675238",
-                    },
-                    {
-                        label: "hello@sibylvision.com",
-                        url: "mailto:support@skyscraper.ai",
-                    },
-                    {
-                        label: "@SibylVision",
-                        url: "//twitter.com/SkyScraperRF",
-                    },
-                ],
-            };
+@Component({
+  name: "Footer",
+  data() {
+    return {
+      links: [
+        {
+          label: "(415) 967-5238",
+          url: "tel:+14159675238"
         },
-    })
-    export default class Footer extends Vue {
-        get year() {
-            return new Date().getFullYear();
+        {
+          label: "hello@sibylvision.com",
+          url: "mailto:support@skyscraper.ai"
+        },
+        {
+          label: "@SkyScraperRF",
+          url: "//twitter.com/SkyScraperRF"
         }
-    }
+      ]
+    };
+  }
+})
+export default class Footer extends Vue {
+  get year() {
+    return new Date().getFullYear();
+  }
+}
 </script>
 
 <style lang="scss">
-    footer {
-        margin-top: -4px;
-        .v-btn {
-            text-transform: none;
-        }
-    }
+footer {
+  margin-top: -4px;
+  .v-btn {
+    text-transform: none;
+  }
+}
 </style>
