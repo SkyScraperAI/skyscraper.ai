e09db7083ad7ba0e81be57f00fbd35ffbe30fa72
Peter Kazazes <peter@peterk.co>
2019-03-25 21:09:58 -0700

    preview page

---
+++ layouts/preview.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app id="sibyl" class="grey lighten-3">
+    <v-toolbar
+      app
+      scroll-off-screen
+      dense
+      clipped-right
+      class="grey lighten-4 elevation-1"
+      style="z-index: 100;"
+      id="navbar"
+      v-if="navVisible"
+    >
+      <v-layout align-center justify-space-between row fill-height>
+        <v-flex grow>
+          <v-toolbar-title>
+            <h1 class="headline">
+              <a href="/">
+                <strong>Sky</strong>Scraper
+              </a>
+              <span class="muted">Preview</span>
+            </h1>
+          </v-toolbar-title>
+        </v-flex>
+        <v-flex shrink fill-height hidden-sm-and-down>
+          <v-toolbar-items v-if="$route.path == '/'" id="toolbar-menu" class="d-flex" justify-end>
+            <v-btn
+              v-for="slide in slides.filter((slide) => slide.name)"
+              flat
+              v-scroll-to="'#' + slide.id"
+              :key="'nav' + slide.id"
+              :aria-label="slide.name"
+            >{{ slide.name }}</v-btn>
+          </v-toolbar-items>
+        </v-flex>
+      </v-layout>
+    </v-toolbar>
+    <v-content>
+      <nuxt/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import { Component } from "vue-property-decorator";
+  import "vuetify/src/stylus/app.styl";
+
+
+  @Component({
+
+  })
+  export default class App extends Vue {
+
+    protected mounted() {
+      this.$store.commit("SET_NAV_VISIBLE", true);
+    }
+
+    get navVisible() {
+      return this.$store.getters.NAV_VISIBLE;
+    }
+  }
+</script>
+
+<style lang="scss">
+  .v-toolbar__title {
+    a {
+      &:active {
+        color: #f44336 !important;
+      }
+
+      color: #f44336 !important;
+      text-decoration: none !important;
+    }
+  }
+
+  h1 {
+    font-size: 21px !important;
+    .headline {
+      .muted {
+        color: #585656;
+      }
+    }
+  }
+
+  #toolbar-menu {
+    .active {
+      position: relative;
+
+      &::before {
+        background-color: currentColor;
+      }
+    }
+  }
+</style>
---
+++ pages/preview.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid pa-0 ma-0 fill-height="">
+    <v-layout row wrap fill-height>
+      <v-flex xs12 fill-height>
+        <iframe fill-height style="width: 100%; height: 100%;" src="https://edge.sibyl.vision"></iframe>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import Vue from "vue";
+  import { Component } from "vue-property-decorator";
+
+  @Component({
+    layout: "preview",
+  })
+  export default class Preview extends Vue {}
+</script>
+
+<style>
+iframe {
+  border-width: 0;
+}
+</style>
